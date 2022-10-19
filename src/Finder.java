108e0392dec0bee9202dac85ba3aa93b29090f46
OsnwHaruki <112005153+OsnwHaruki@users.noreply.github.com>
2022-10-20 01:55:27 +0300

    Update Finder.java

--- src/Finder.java
+++ src/Finder.java
@@ -5,7 +5,6 @@ public class Finder {
     public static void main(String[] args) throws IOException {
         ImportBase repository = new ImportBase();
         ArrayList<String> words = new ArrayList<>(repository.ListGet("https://raw.githubusercontent.com/Harrix/Russian-Nouns/main/dist/russian_nouns.txt"));
-        System.out.println(words.get(1));
         WordSort wordSort = new WordSort();
         ArrayList<String> goodWords = new ArrayList<>(wordSort.sortGoodWords(words));
         for(String s : goodWords){
